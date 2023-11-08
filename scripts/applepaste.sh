b4a3895df0d1916f9db4e44fffef4b6862fe883b
moatnp <maartenpentinga@protonmail.com>
2023-11-08 11:49:23 +0100

    Update applepaste.sh

--- scripts/applepaste.sh
+++ scripts/applepaste.sh
@@ -3,9 +3,9 @@
 ARGV="$1"
 
 #Enter location of your scripts
-TOOLS="${HOME}/Bitbar-paste-db/scripts"
+TOOLS="${HOME}/Bitbar-paste-DB/scripts"
 
 #enter the location of your 'pastedb'
-PASTEDB="${HOME}/Bitbar-paste-db/pastedb"
+PASTEDB="${HOME}/Bitbar-paste-DB/pastedb"
 
 /usr/bin/osascript "${TOOLS}/paste.applescript" ${ARGV}
